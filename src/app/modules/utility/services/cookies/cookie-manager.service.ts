2814be5bb9766f3e90bb1b553c84b29370cd8d86
ozen-hub <info.ozenhub@gmail.com>
2024-02-07 21:44:17 +0530

    3

--- src/app/modules/utility/services/cookies/cookie-manager.service.ts
+++ src/app/modules/utility/services/cookies/cookie-manager.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {CookieService} from "ngx-cookie-service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CookieManagerService {
 
-  constructor() { }
+  constructor(private cookieService: CookieService) {
+  }
+
+
+  public createToken(value: any) {
+    this.cookieService.set('my-token', value, 30, '/');
+  }
+
+  public isTokenExists(): boolean {
+    return this.cookieService.check('my-token');
+  }
+
+  public removeToken() {
+    if (this.isTokenExists()) {
+      this.cookieService.delete('my-token');
+    }
+  }
+
+
 }
--- src/app/pages/security/login-page/login-page.component.ts
+++ src/app/pages/security/login-page/login-page.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import {Component} from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
+import {AuthService} from "../../../modules/utility/services/auth/auth.service";
+import {response} from "express";
+import {CookieManagerService} from "../../../modules/utility/services/cookies/cookie-manager.service";
 
 @Component({
   selector: 'app-login-page',
@@ -11,5 +14,15 @@ import {RouterLink} from "@angular/router";
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+
+  constructor(private authService: AuthService, private cookieService:CookieManagerService, private router:Router) {
+  }
+
+  login() {
+    this.authService.getToken().subscribe(response => {
+      this.cookieService.createToken(response.data);
+      this.router.navigateByUrl('/');
+    })
+  }
 
 }
