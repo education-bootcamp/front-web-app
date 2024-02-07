da6bcee0f310c6843bb736f716fb3d8e35e6e60b
ozen-hub <info.ozenhub@gmail.com>
2024-02-07 22:07:12 +0530

    4

--- src/app/pages/security/login-page/login-page.component.ts
+++ src/app/pages/security/login-page/login-page.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router, RouterLink} from "@angular/router";
 import {AuthService} from "../../../modules/utility/services/auth/auth.service";
 import {response} from "express";
@@ -13,10 +13,18 @@ import {CookieManagerService} from "../../../modules/utility/services/cookies/co
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit{
+
+
 
   constructor(private authService: AuthService, private cookieService:CookieManagerService, private router:Router) {
   }
+
+  ngOnInit(): void {
+       if (this.cookieService.isTokenExists()){
+         this.router.navigateByUrl('/');
+       }
+    }
 
   login() {
     this.authService.getToken().subscribe(response => {
